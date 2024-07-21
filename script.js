7877c7da75ffaf95ab6f7102742aa955f5a0fad6
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2024-07-21 03:45:39 -1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function(){
+	console.log('Page has loaded');
+	document.getElementById('loading-page').style.display = 'none';
+	console.log('loading page hidden');
+	document.getElementById('main').style.display = 'block';
+	console.log('displaying content');
+});
+
+
+window.addEventListener("scroll", function(){
+	var header = document.getElementById("cn-nav");
+	header.classList.toggle("cn-sticky", window.scrollY > 0);
+});
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target); // Stop observing the element after the class is added
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
